ea67bf12883ce8964783bed8ba8901ca3993901b
qiuyukun <qiuyukunjnu@163.com>
2018-03-23 13:38:58 +0800

    fixed a bug in cifar10_eval

--- cifar10_eval.py
+++ cifar10_eval.py
@@ -32,32 +32,31 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
         else:
             print('No checkpoint file found!')
             return
-    coord = tf.train.Coordinator()
-    try:
-        threads = []
-        for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
-            threads.append(qr.create_threads(sess, coord=coord, daemon=True,
-                                             start=True))
-        num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size ))
-        true_count = 0
-        total_sample_count = num_iter * FLAGS.batch_size
-        step = 0
-        while step < num_iter and not coord.should_stop():
-            predictions = sess.run([top_k_op])
-            true_count += np.sum(predictions)
-            step += 1
-        precision = true_count / total_sample_count
-        print('%s: precision @1 = %.3f' % (datetime.now(), precision))
+        coord = tf.train.Coordinator()
+        try:
+            threads = []
+            for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+                threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+                                                 start=True))
+            num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+            true_count = 0
+            total_sample_count = num_iter * FLAGS.batch_size
+            step = 0
+            while step < num_iter and not coord.should_stop():
+                predictions = sess.run([top_k_op])
+                true_count += np.sum(predictions)
+                step += 1
+            precision = true_count / total_sample_count
+            print('%s: precision @1 = %.3f' % (datetime.now(), precision))
 
-        summary = tf.Summary()
-        summary.ParseFromString(sess.run(summary_op))
-        summary.value.add(tag='Precision @ 1', simple_value=precision)
-        summary_writer.add_summary(summary, global_step)
-    except Exception as e:
-        coord.request_stop(e)
-
-    coord.request_stop()
-    coord.join(threads, stop_grace_period_secs=10)
+            summary = tf.Summary()
+            summary.ParseFromString(sess.run(summary_op))
+            summary.value.add(tag='Precision @ 1', simple_value=precision)
+            summary_writer.add_summary(summary, global_step)
+        except Exception as e:
+            coord.request_stop(e)
+        coord.request_stop()
+        coord.join(threads, stop_grace_period_secs=10)
 
 
 def evaluate():
@@ -81,7 +80,7 @@ def evaluate():
             eval_once(saver, summary_writer, top_k_op, summary_op)
             if FLAGS.run_once:
                 break
-            time.sleep(FLAGS.eval_internal_secs)
+            time.sleep(FLAGS.eval_interval_secs)
 
 
 def main(argv=None):
@@ -94,5 +93,3 @@ def main(argv=None):
 
 if __name__ == '__main__':
     tf.app.run()
-
-
